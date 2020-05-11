ede04e8fea601846a3367bde817b4b84e4b4e16f
caposerenity <1464390895@qq.com>
2020-05-11 19:42:57 +0800

    fix router

--- front_end/src/components/layout/index.vue
+++ front_end/src/components/layout/index.vue
@@ -3,6 +3,9 @@
         <a-layout-header id="components-layout-demo-fixed">
             <Header></Header>
         </a-layout-header>
+        <a-layout-content id="main_content">
+            <router-view></router-view>
+        </a-layout-content>
     </a-layout>
 </template>
 
@@ -24,5 +27,8 @@ export default {
   height: 10%;
   background: rgba(0, 0, 0, 0.65);
   margin: 0% 0% 2% 0%;
+}
+#main_content{
+    margin: 5% 0 0 0;
 }
 </style>
--- front_end/src/router/index.js
+++ front_end/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Layout from '../components/layout/index.vue'
-
+import Setting from '../views/person_setting.vue'
 
 Vue.use(VueRouter)
 
@@ -11,18 +11,29 @@ Vue.use(VueRouter)
       path:'/',
       name:'layout',
       component:Layout,
+      children:[
+       {
+         path:'/setting',
+         name:'setting',
+         component:Setting,
+       },
+       {
+        path:'/home',
+        name:'home',
+        component:Home,
+      },    
+    
+      ]
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
