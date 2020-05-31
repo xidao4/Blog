8753595d485a9d8642105457425f3f7030ab7732
Chenshasan <48911319+Chenshasan@users.noreply.github.com>
2020-05-31 21:28:33 +0800

    feat: 后端数据库

--- back_end/sql/blog.sql
+++ back_end/sql/blog.sql
@@ -5,10 +5,6 @@
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`blog_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `blog_db`;
-
 
 /*Table structure for table `tb_passage` */
 
@@ -27,3 +23,24 @@ CREATE TABLE `tb_passage` (
 /*Data for the table `tb_passage` */
 
 insert  into `tb_passage`(`id`,`title`,`content`,`is_deleted`,`create_time`,`update_time`) values (1,'我是十三','## About me\n\n我是十三，一名Java开发者，技术一般，经历平平，但是也一直渴望进步，同时也努力活着，为了人生不留遗憾，也希望能够一直做着自己喜欢的事情，得闲时分享心得、分享一些浅薄的经验，等以后老得不能再老了，就说故事已经讲完了,不去奢求圆满。\n\n相信浏览这段话的你也知道，学习是一件极其枯燥而无聊的过程，甚至有时候显得很无助，我也想告诉你，成长就是这样一件残酷的事情，任何成功都不是一蹴而就，需要坚持、需要付出、需要你的毅力，短期可能看不到收获，因为破茧成蝶所耗费的时间不是一天。\n\n## Contact\n\n- 我的邮箱：2449207463@qq.com\n- QQ技术交流群：796794009\n- 我的网站：http://13blog.site\n\n## Quote\n\n- Steve Jobs\n\n> Stay hungry,Stay foolish\n\n- Kahlil Gibran\n\n>The FIRST TIME WHEN I saw her being meek that she might attain height.<br>\nThe SECOND TIME WHEN I saw her limping BEFORE the crippled.<br>\nThe third TIME WHEN she was given TO choose BETWEEN the hard AND the easy, AND she chose the easy.<br>\nThe fourth TIME WHEN she COMMITTED a wrong, AND comforted herself that others also COMMIT wrong.<br>\nThe fifth TIME WHEN she forbore FOR weakness, AND attributed her patience TO strength.<br>\nThe sixth TIME WHEN she despised the ugliness of a face, AND knew NOT that it was ONE of her own masks.<br>\nAND the seventh TIME WHEN she sang a song of praise, AND deemed it a virtue.',0,'2017-03-12 00:31:15','2018-11-12 00:31:15');
+
+DROP TABLE IF EXISTS `tb_Comment`;
+
+CREATE TABLE `tb_Comment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '评论Id',
+  `passageId` bigint(20) NOT NULL COMMENT '对应文章Id',
+  `userId` bigint(20) NOT NULL COMMENT '评论者Id',
+  `content` mediumtext NOT NULL COMMENT '评论内容',
+  `commentTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '评论时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `tb_User`;
+
+CREATE TABLE `tb_User` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户Id',
+  `email` varchar(200) NOT NULL COMMENT '邮箱',
+  `password` varchar(200) NOT NULL COMMENT '密码',
+  `userName` varchar(200) NOT NULL COMMENT '用户名',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
