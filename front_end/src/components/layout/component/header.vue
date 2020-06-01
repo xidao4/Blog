9102dd2b9e91449d57058d3957644bbacd111629
lenovo <181250077@smail.nju.edu.cn>
2020-06-01 20:49:35 +0800

    add: 三个静态页面

--- front_end/src/components/layout/component/header.vue
+++ front_end/src/components/layout/component/header.vue
@@ -1,83 +1,84 @@
 <template>
   <div>
     <a-row :gutter="16">
-    <a-col :span="4 " :order="1"><div class="logo"></div></a-col>
-    <a-col :span="16"  :order="2">
-    <a-menu v-model="current" id="up_menu" theme="dark" mode="horizontal" >
-      <a-menu-item key="main"> <a-icon type="appstore" />首页</a-menu-item>
-      <a-menu-item key="app" > <a-icon type="profile" />我的收藏</a-menu-item>
-      <a-sub-menu>
+      <a-col :span="4 " :order="1"><div class="logo"></div></a-col>
+      <a-col :span="16"  :order="2">
+        <a-menu v-model="current" id="up_menu" theme="dark" mode="horizontal" >
+          <a-menu-item key="main"> <a-icon type="appstore" />首页</a-menu-item>
+          <a-menu-item key="app" > <a-icon type="profile" />我的收藏
+            <router-link :to="'searchResult'"></router-link>
+          </a-menu-item>
+          <a-sub-menu>
         <span slot="title" class="submenu-title-wrapper"
-          ><a-icon type="star" />我的博客</span
+        ><a-icon type="star" />我的博客</span
         >
+            <a-menu-item key="person">
+              <a-icon type="edit" />
+              写博客
+              <router-link :to="'createblog'"></router-link>
+            </a-menu-item>
+            <a-menu-item key="setting:2">
+              Option 2
+            </a-menu-item>
+            <a-menu-item key="setting:3">
+              Option 3
+            </a-menu-item>
+            <a-menu-item key="setting:4">
+              Option 4
+            </a-menu-item>
+          </a-sub-menu>
           <a-menu-item key="person">
-          <a-icon type="edit" />
-            写博客
-            <router-link :to="'createblog'"></router-link>
-        </a-menu-item>
-          <a-menu-item key="setting:2">
-            Option 2
+            <a-icon type="setting" />
+            个人中心
+            <router-link :to="'setting'"></router-link>
           </a-menu-item>
-          <a-menu-item key="setting:3">
-            Option 3
-          </a-menu-item>
-          <a-menu-item key="setting:4">
-            Option 4
-          </a-menu-item>
-      </a-sub-menu>
-      <a-menu-item key="person">
-        <a-icon type="setting" />
-          个人中心
-          <router-link :to="'setting'"></router-link>
-      </a-menu-item>
-      
-    </a-menu>
-    </a-col>
+
+        </a-menu>
+      </a-col>
 
       <a-col  :span="4"  :order="3" id="search">
         <a-input-search
-          enter-button="Search"
-          size="large"
-          @search="onSearch"
-          >
-          </a-input-search>
+                enter-button="Search"
+                size="large"
+                @search="onSearch"
+        >
+        </a-input-search>
       </a-col>
     </a-row>
   </div>
 </template>
 
 <script>
-export default {
+  export default {
     name:'Header',
     data() {
-        return {
-            current: ['main'],
-        };
+      return {
+        current: ['main'],
+      };
     },
     methods: {
       onSearch(value) {
-      
       },
     },
-};
+  };
 </script>
 <style scoped>
-.logo {
-  width: 5%;
-  height: 60%;
-  background: rgba(255, 255, 255, 0.2);
-  float: left;
-}
-#up_menu{
-  height: 100%;
-  background: rgba(255, 255, 255, 0);
-  padding: 1.4% 0 0 0;
-  font-size: 120%;
-  text-align: right;
-}
-#search{
-  padding: 1.2% 0 0 0%;
-  text-align: right;
-  float: right;
-}
+  .logo {
+    width: 5%;
+    height: 60%;
+    background: rgba(255, 255, 255, 0.2);
+    float: left;
+  }
+  #up_menu{
+    height: 100%;
+    background: rgba(255, 255, 255, 0);
+    padding: 1.4% 0 0 0;
+    font-size: 120%;
+    text-align: right;
+  }
+  #search{
+    padding: 1.2% 0 0 0%;
+    text-align: right;
+    float: right;
+  }
 </style>
---
+++ front_end/src/views/MyBlog/myCollection.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="collection" >
+        <a-list item-layout="horizontal" size="large">
+            <a-list-item slot="renderItem" slot-scope="item" key="item.id" >
+                <a-list-item-meta>
+                    <a slot="title" @click=jumpToDetails(item.id) style="position:relative;top: 10px;left:45px;font-size: 200%">{{item.title}}</a>
+                    <span  slot="description" style="position:absolute;margin-top:30px;margin-left:40px;color: black;white-space: pre">{{item.details}}}<br><br></span>
+                    <a-row>
+                        <a-col style="color: darkgray">{{item.userId}} 编辑于 {{item.time}}</a-col>
+                        <a-col style="color: darkgray"><a-button @click="deleteCollection(item.id)">取消收藏</a-button></a-col>
+                    </a-row>
+                </a-list-item-meta>
+            </a-list-item>
+        </a-list>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "myCollection"
+    }
+</script>
+
+<style scoped>
+
+</style>
