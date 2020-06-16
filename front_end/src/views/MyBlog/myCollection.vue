01cd37a8da57c4e623fc7e404703afeaf1afbc40
lenovo <181250077@smail.nju.edu.cn>
2020-06-16 13:41:41 +0800

    fix: 收藏夹优化

--- front_end/src/views/MyBlog/myCollection.vue
+++ front_end/src/views/MyBlog/myCollection.vue
@@ -3,23 +3,31 @@
         <br>
         <a-list item-layout="horizontal" size="large" v-bind:data-source="collection" style="text-align: left;margin-left: 150px;margin-right: 150px">
             <a-list-item slot="renderItem" slot-scope="item" key="item.id" >
-                <row style="text-align: left;margin-left: 100px;margin-right: 200px">
+                <row style="text-align: left;margin-left: 200px;margin-right: 200px;">
                     <br>
-                    <a slot="title"  @click="jumpToDetails(item.id)" style="text-align: left;font-size: 200%;color:gray" v-if="item.title.length<10">
+                    <a slot="title"  @click="jumpToDetails(item.id)" style="text-align: left;font-size: 200%;color:gray" v-if="item.title.length<13">
                         {{item.title}}
                     </a>
                     <a slot="title"  @click="jumpToDetails(item.id)" style="text-align: left;font-size: 200%;color:gray" v-else>
-                        {{item.title.substring(0,10)}}...
+                        {{item.title.substring(0,13)}}...
                     </a>
-                    <a-row style="text-align: left;font-size: 120%;overflow:hidden;text-overflow:ellipsis;margin-top: 20px" @click="jumpToDetails(item.id)"  v-if="item.content.length>100">
-                        {{item.content.substring(0,100)}}...
+                    <a-row style="text-align: left;font-size: 120%;overflow:hidden;text-overflow:ellipsis;margin-top: 20px" @click="jumpToDetails(item.id)" >
+                        <a-col span="5">
+                            <img src="../../assets/post-bg-swift.jpg" height="100" width="120"/>
+                        </a-col>
+                        <a-col  v-if="item.content.length>100">
+                            <span>    {{item.content.substring(0,100)}}...</span>
+                        </a-col>
+                        <a-col style="margin-left: 140px" v-else>
+                            <span>{{item.content}}</span>
+                        </a-col>
                     </a-row>
-                    <a-row style="text-align: left;font-size: 120%;overflow:hidden;text-overflow:ellipsis;margin-top: 20px" @click="jumpToDetails(item.id)" v-else>
-                        {{item.content}}
-                    </a-row>
-                    <a-row style="margin-top: 20px">
+                    <a-row>
                         <a-col style="color: darkgray;text-align: left" :span="12"><br>创建于 {{item.createTime.substring(0,10)}}</a-col>
-                        <a-col style="color: darkgray;text-align: right;margin-top: 10px" :span="12"><a-button  @click="delCollection(item.id) " style="color: #192c3e"><a-icon type="star"></a-icon>取消收藏</a-button></a-col>
+                        <a-col style="color: darkgray;text-align: left;margin-left: 500px" :span="12"><a-button  @click="delCollection(item.id) " style="color: #192c3e"><a-icon type="star"></a-icon>取消收藏</a-button></a-col>
+                    </a-row>
+                    <a-row>
+                        <hr style="width: 100%;margin-top: 10px">
                     </a-row>
                 </row>
             </a-list-item>
